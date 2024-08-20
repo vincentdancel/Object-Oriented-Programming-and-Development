ffbef0f408623e292ed6e78277b530615738f590
Vince Dancel <142908288+vincentdancel@users.noreply.github.com>
2024-08-20 12:53:10 +0800

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,24 @@
+
+package javaapplication2;
+
+public class JavaApplication2 {
+
+    // encapsulation
+    
+    private String color;
+    private String brand;
+    
+    public void SetofColor (String color) {
+        this.color = color;
+        
+    }
+    public String getColor () {
+        return this.color;
+    }
+    
+}
+
+
+    
+    
+
