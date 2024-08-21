6911cefce5162092fed8d0f2cd9471b5fcbd8c77
Vince Dancel <142908288+vincentdancel@users.noreply.github.com>
2024-08-21 10:56:04 +0800

    Update PizzaENCAPSULATION.java

--- PizzaENCAPSULATION.java
+++ PizzaENCAPSULATION.java
@@ -45,3 +45,30 @@ public class Pizza {
         return price;
     }
 }
+-----------------------------------------------------------------------------------------------
+
+package com.mycompany.pizza;
+
+import javax.swing.JOptionPane;
+
+public class TestPizza {
+
+    public static void main(String[] args) {
+
+        Pizza n = new Pizza("peperoni", 12, 13.99);
+
+        JOptionPane.showMessageDialog(null, "Set of Toppings: " + n.getToppings()
+                + "\nSet of Diameter: " + n.getDiameter()
+                + "\nSet price: " + n.getPrice());
+
+        n.setToppings("peperoni");
+        n.setDiameter(12);
+        n.setPrice(13.99);
+
+        JOptionPane.showMessageDialog(null, "Get Toppings: " + n.getToppings()
+                + "\nGet Diameter: " + n.getDiameter()
+                + "\nThe price: " + n.getPrice());
+
+    }
+
+}
